a6afbe3f835e5596900f9c21f2f0f996265270d3
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-03-12 09:50:58 +0545

    first commit

---
+++ src/context/index.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+export const GlobalContext = createContext(null);
+
+export default function GlobalState({ children}) {
+  const [showNavModel, setShowNavModel]= useState(false);
+
+  return (
+    <GlobalContext.Provider value={{ showNavModel, setShowNavModel }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
